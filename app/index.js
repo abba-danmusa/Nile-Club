fcfeced0aff6db88ec0980ceded3868d247597de
Abba Danmusa <danmusaabdullahi@gmail.com>
2023-11-08 16:44:46 +0100

    First Commit

--- app/index.js
+++ app/index.js
@@ -5,27 +5,44 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RootSiblingParent } from 'react-native-root-siblings'
 
 const queryClient = new QueryClient()
-
-export default function App() {
+export default function Page() {
   return (
     <SafeAreaProvider>
       <RootSiblingParent>
         <QueryClientProvider client={queryClient}>
           <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <View style={styles.container}>
+              <View style={styles.main}>
+                <Text style={styles.title}>Hello World</Text>
+                <Text style={styles.subtitle}>This is the first page of your app.</Text>
+              </View>
+            </View>
             <StatusBar style="auto" />
           </View>
         </QueryClientProvider>
       </RootSiblingParent>
     </SafeAreaProvider>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
   },
 });
