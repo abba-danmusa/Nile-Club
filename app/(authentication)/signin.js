537f25ed9b055aca31ffed63237a94d7a31d87ae
Abba Danmusa <danmusaabdullahi@gmail.com>
2024-01-02 00:31:03 +0100

    Style signin screen

--- app/(authentication)/signin.js
+++ app/(authentication)/signin.js
@@ -1,9 +1,15 @@
+import React, { useState } from'react'
 import { Button, Input, Text } from '@rneui/themed'
 import { Link } from 'expo-router'
 import { useAuthStore } from '../../hooks/stores/useAuthStore'
 import { useSignin } from '../../hooks/queries/useAuthentication'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 
 export default function Signin() {
+  const [showPassword, setShowPassword] = useState(false)
   const { email, password, setEmail, setPassword } = useAuthStore()
   const { mutate: signin } = useSignin()
   
@@ -11,13 +17,75 @@ export default function Signin() {
     signin({email, password})
   }
 
+  const secureTextEntry = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const eye = {
+    true:  <AntDesign name="eyeo" size={30} color="black" />,
+    false: <AntDesign name="eye" size={30} color="black" />
+  }[showPassword]
+
   return (
-    <>
-      <Text>Sign in</Text>
-      <Input label={'email'} value={email} onChangeText={setEmail}/>
-      <Input label={'password'} value={password} onChangeText={setPassword}/>
+    <SafeAreaView>
+      <StatusBar style="auto" hidden/>
+      <Text style={styles.header}>Hello</Text>
+      <Text style={styles.subheader}>Sign in to your account</Text>
+      <Input
+        value={email}
+        onChangeText={setEmail}
+        placeholder='email'
+        autoCapitalize='none'
+        autoCompleteType='email'
+        autoCorrect={false}
+        keyboardType='email-address'
+        returnKeyType='next'
+        leftIcon={() => <AntDesign name="user" size={30} color="black" />}
+      />
+      <Input
+        value={password}
+        onChangeText={setPassword}
+        placeholder='password'
+        secureTextEntry={showPassword ? false : true}
+        autoCapitalize='none'
+        autoCompleteType='password'
+        autoCorrect={false}
+        returnKeyType='go'
+        leftIcon={() => <AntDesign name="lock" size={30} color="black" />}
+        rightIcon={() => {
+          return (
+            <TouchableOpacity
+              onPress={secureTextEntry}
+              style={{
+                padding: 5
+              }}
+            >
+              {eye}
+            </TouchableOpacity>
+          )
+        }}
+
+      />
       <Button title={'Submit'} onPress={signup}/>
       <Link href="/signup">Sign up</Link>
-    </>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 50,
+    marginBottom: 10,
+    marginTop: 100,
+    alignSelf: 'center',
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  subheader: {
+    fontSize: 20,
+    marginBottom: 10,
+    alignSelf: 'center',
+    color: '#333',
+    fontWeight: 'bold',
+  }
+})
--- utils/axios.js
+++ utils/axios.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
   // baseURL: 'https://9t396f1p-5555.euw.devtunnels.ms/'
-  baseURL: 'https://3143-102-91-4-134.ngrok-free.app' 
+  baseURL: 'https://2f6f-197-210-52-13.ngrok-free.app' 
 })
 
 instance.interceptors.request.use(
